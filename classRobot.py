00df5d06101ac4b517cbd840613f64aceec8a171
yasmineoudjani <55353378+yasmineoudjani@users.noreply.github.com>
2020-02-11 13:58:59 +0100

    Update classRobot.py

--- classRobot.py
+++ classRobot.py
@@ -12,31 +12,31 @@ class Robot:
 		self.y = self.y + self.sensY
 
 	def tourner(self,s):
-	#s est le paramètre permettant de savoir dans quel sens on tourne , 0 sens horaire , 1 sens anti-horaire
-		if(s=0):
-			if(x=1 and y=0):
+	
+		if(s==0):
+			if(x==1 and y==0):
 				self.sensX = 0
 				self.sensY = 1
-			if(x=0 and y=1):
+			if(x==0 and y==1):
 				self.sensX = -1
 				self.sensY = 0
-			if(x=-1 and y=0):
+			if(x==-1 and y==0):
 				self.sensX = 0
 				self.sensY = -1
-			if(x=0 and y=-1):
+			if(x==0 and y==-1):
 				self.sensX = 1
 				self.sensY = 0
-		if(s=1):
-			if(x=0 and y=-1):
+		if(s==1):
+			if(x==0 and y==-1):
 				self.sensX = -1
 				self.sensY = 0
-			if(x=-1 and y=0):
+			if(x==-1 and y==0):
 				self.sensX = 0
 				self.sensY = 1
-			if(x=0 and y=1):
+			if(x==0 and y==1):
 				self.sensX = 1
 				self.sensY = 0
-			if(x=1 and y=0):
+			if(x==1 and y==0):
 				self.sensX = 0
 				self.sensY = -1
 
