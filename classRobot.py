3123b6ec53a8c72782305d7b713de403e94d1235
alexisquest <alexisquest@users.noreply.github.com>
2020-02-16 04:36:13 +0300

    Update classRobot.py

--- classRobot.py
+++ classRobot.py
@@ -15,20 +15,24 @@ class Robot:
                 self.alpha = 0
 
         def avancer(self):
-                self.x += vX
-                self.y += vY
-                case_x = int(x)
-                case_y = int(y)
+                self.x += self.vX
+                self.y += self.vY
+                self.case_x = int(self.x)
+                self.case_y = int(self.y)
+                print("le robot avance vx=", self.vX , " vy=" , self.vY , "\n pox=" , self.x , " posy=" , self.y)
 
         def tourner(self,angle):
-		self.vX = vX*math.cos(angle) - self.vY*math.sin(angle)
-                self.vY = vX*math.sin(angle) + self.vY*math.cos(angle)
-                alpha += angle%360
+                vX = self.vX
+                vY = self.vY
+                self.vX = vX*math.cos(angle) - vY*math.sin(angle)
+                self.vY = vX*math.sin(angle) + vY*math.cos(angle)
+                self.alpha += angle%360
+                print("le robot tourne. nouvelle angle : ", self.alpha)
 
         def changementVitesse(self,d):
-                if(vX == 0.0 && vY == 0.0):
-                        vX= math.cos(self.alpha)*d
-                        vY= math.sin(self.alpha)*d
+                self.vX= math.cos(self.alpha)*d
+                self.vY= math.sin(self.alpha)*d
+                print("le robot change de vitesse")
 
         def getCaseX(self):
                 return self.case_x
@@ -49,4 +53,4 @@ class Robot:
                 return self.vY
 
         def getAlpha(self):
-                return alpha
+                return self.alpha
