0089992cdb8c25a4421b0744107a1417d645b554
alexisquest <alexisquest@users.noreply.github.com>
2020-02-16 04:36:05 +0300

    Update classCapteur.py

--- classCapteur.py
+++ classCapteur.py
@@ -1,4 +1,4 @@
-import classArene
+from classArene import *
 
 class Capteur:
 	"""classe permettant de créer un capteur de distance"""
@@ -19,7 +19,7 @@ class Capteur:
 		vy = int(self.robot.getVY())
 
 		#si le robot est à l'arrêt , on regarde si il y a un obstacle à une case en avant
-		if(vx == 0 && vy == 0 ):
+		if(vx == 0 and vy == 0 ):
 			vx=1
 			vy=1
 
@@ -29,26 +29,32 @@ class Capteur:
 			#on regarde les vx prochaine cases
 			for i in range(0,vx):
 				#si une de ces cases contiennent un obstacle on retourne true
-				if (structure[self.x+1][self.y] == 1):
-					return true
+				if (structure[(self.x)+1][self.y] == '1'):
+					print("obstacle devant le robot")
+					return True
 
 		#si le robot est trouné vers la haut
 		if (alpha == 90):
 			for i in range(0,vy):
-				if (structure[self.x][self.y+1] == 1):
-					return true
+				if (structure[self.x][(self.y)+1] == 1):
+					print("obstacle devant le robot")
+					return True
 
 		#si le robot est trouné vers la gauche
 		if (alpha == 180):
 			for i in range(0,vx):
-				if (structure[self.x-1][self.y] == 1):
-					return true
+				if (structure[(self.x)-1][self.y] == 1):
+					print("obstacle devant le robot")
+					return True
 
 		#si le robot est trouné vers le bas
 		if (alpha == 270):
 			for i in range(0,vy):
-				if (structure[self.x][self.y-1] == 1):
-					return true
+				if (structure[self.x][(self.y)-1] == 1):
+					print("obstacle devant le robot")
+					return True
+
+		print("pas d'obstacle devant le robot")
 
 
 
