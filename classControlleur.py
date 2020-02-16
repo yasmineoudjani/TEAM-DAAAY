8389caf2a5e56fe588e631f1008147aa7b12ad03
alexisquest <alexisquest@users.noreply.github.com>
2020-02-16 04:36:08 +0300

    Update classControlleur.py

--- classControlleur.py
+++ classControlleur.py
@@ -1,48 +1,55 @@
-import classRobot
-import classCapteur
-import classArene
 import random
 import math
+
+from classRobot import *
+from classArene import *
+from classCapteur import *
+
 
 class Controlleur:
 	""" classe premettant de créer un controlleur"""
 
 	def __init__(self):
+		self.id=1
 
-	def alea(self,capteur,arene):
-
+	def alea(self,capteur,arene,robot):
 		#si le robot est à l'arret 
-		if(robot.getVX == 0 && robot.getVY == 0):
+		if(robot.getVX() == 0.0 and robot.getVY() == 0.0):
+			print("le robot est à l'arrêt")
 			#on vérifie qu'il n'y a rien devant le robot
-			if(not(capteur.obstacle(arene))):
+			if(not(capteur.Obstacle(arene))):
 				#on lui donne une vitesse de 1 case par unité de temps
 				robot.changementVitesse(1)
 			else:
 				#on le fait tourner aléatoirement
 				c=random.randint(0, 3)
-				robot.tourner(c*(pi/2))
+				robot.tourner(c*(math.pi/2))	
 
 		else:
 			#si il y a un obstacle sur la trajectoire du robot
-			if(capteur.obstacle(arene)):
+			print("deuxieme cas")
+			if(capteur.Obstacle(arene)):
 				#on arrete le robot
 				robot.changementVitesse(0)
-
+		robot.avancer()
 	def sortieLab(self,capteur,arene):
 
 		#si le robot est à l'arret 
-		if(robot.getVX == 0 && robot.getVY == 0):
+		if(robot.getVX() == 0 and robot.getVY() == 0):
 			#on vérifie qu'il n'y a rien devant le robot
 			if(not(capteur.obstacle(arene))):
 				#on lui donne une vitesse de 1 case par unité de temps
 				robot.changementVitesse(1)
+				return
 			else:
 				#on le fait tourner de 90 degré 
-				robot.tourner(pi/2)
+				robot.tourner(math.pi/2)
+				return
 
 		else:
 			#si il y a un obstacle sur la trajectoire du robot
-			if(capteur.obstacle(arene)):
+			if(capteur.Obstacle(arene)):
 				#on arrete le robot
 				robot.changementVitesse(0)
+				return
 
