41944ba04869fe469855b7ade308fc77e3ac98a7
alexisquest <alexisquest@users.noreply.github.com>
2020-05-18 18:43:29 +0300

    version avec fct carre

--- controleur.py
+++ controleur.py
@@ -11,6 +11,46 @@ class Controleur:
 
 	def __init__(self,robot):
 		self.robot=robot
+		self.compteur=0
+		self.compteur_nb_cote=0
+
+	def set_motor_dps(self,dps):
+		self.robot.changementVitesse(15)
+		
+	def tourner(self,angle):
+		self.robot.tourner(angle)
+
+	def stop(self,capteur):
+		if(capteur.distanceObstacle()<=distance_arret):
+			#on arrete le robot
+			self.robot.changementVitesse(0)
+			return 1
+
+	def avancer(self,capteur):
+		if((int)(self.robot.vecteurVitesse.v)==0):
+			#on vérifie qu'il n'y a rien devant le robot
+			if(capteur.distanceObstacle()>distance_arret):
+				#on lui donne une vitesse de 1 case par unité de temps
+				self.robot.changementVitesse(15)
+				self.compteur += 1
+		else:
+			#si il y a un obstacle sur la trajectoire du robot
+			if(self.stop(capteur) != 1):
+				self.compteur += 1
+
+	def carre(self , longueur , capteur):
+		if(self.compteur_nb_cote < 4):
+			if(self.compteur == longueur):
+				self.set_motor_dps(0)
+				self.compteur = 0
+				self.tourner(math.pi/2)
+				self.compteur_nb_cote += 1
+			else:
+				self.avancer(capteur)
+			return 0
+		else:
+			return 1
+
 
 	def alea(self,capteur,arene):
 		#si le robot est à l'arret
@@ -26,9 +66,7 @@ class Controleur:
 
 		else:
 			#si il y a un obstacle sur la trajectoire du robot
-			if(capteur.distanceObstacle()<=distance_arret):
-				#on arrete le robot
-				self.robot.changementVitesse(0)
+			self.stop(capteur)
 
 	def aleaLarge(self,capteur,arene):
 		#si le robot est à l'arret
@@ -44,12 +82,6 @@ class Controleur:
 
 		else:
 			#si il y a un obstacle sur la trajectoire du robot
-			if(capteur.distanceObstacleLarge()<=distance_arret):
-				#on arrete le robot
-				self.robot.changementVitesse(0)
+			self.stop(capteur)
 
-	def set_motor_dps(self,dps):
-		self.robot.changementVitesse(15)
-		
-	def tourner(self,angle):
-		self.robot.tourner(angle)
+
